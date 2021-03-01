9c499f477d3c8f2bc690c8ee277285743ed3504d
Valeria Revolledo <valeriadesiree81@gmail.com>
2021-03-01 15:10:48 -0400

    modificated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='mlproject',
+setup(name='mlproject_vale',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
@@ -13,5 +13,5 @@ setup(name='mlproject',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/mlproject-run'],
+      scripts=['scripts/mlproject-run', 'scripts/mlproject-computedist'],
       zip_safe=False)
